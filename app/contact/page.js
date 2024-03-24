aede15e4b693a16bfa240d5cb447c7c8e25faed5
ProcessX <luc2526@gmail.com>
2024-03-24 14:55:03 +0100

    first commit

---
+++ app/contact/page.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+function page() {
+  return (
+    <main>
+      <section>
+        <div>
+          <h1>
+            <span>Want to reach out?</span>
+            <span>Find me here</span>
+          </h1>
+        </div>
+        <div>
+          <span>Contact button</span>
+        </div>
+        <div>
+          <p>Or check my socials</p>
+          <ul>
+            <li>
+              <span>Social icon</span>
+            </li>
+          </ul>
+        </div>
+      </section>
+    </main>
+  );
+}
+
+export default page;
