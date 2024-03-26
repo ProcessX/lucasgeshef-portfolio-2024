2beadbb904c5501630f5d11fc5d7f8c4b3c86275
ProcessX <luc2526@gmail.com>
2024-03-26 21:36:43 +0100

    links hover activate

--- app/page.js
+++ app/page.js
@@ -6,11 +6,11 @@ export default function Home() {
 
       <section className="">
         <div className="containerRaw">
-          <h1 className="c-font-4xl mt-[160px] tablet:mt-[192px] mb-[88px] tablet:mb-[128px]">
-            <span className="block mb-12 tablet:mb-16">Hi.</span>
-            <span className="block">I'm a</span>
-            <span className="block">graphic designer & dev</span>
-            <span className="block">based in Belgium</span>
+          <h1 className="c-font-4xl mt-[160px] tablet:mt-[192px] mb-[88px] tablet:mb-[128px] tablet:grid tablet:grid-cols-12">
+            <span className="block mb-12 tablet:mb-0 tablet:col-span-2">Hi.</span>
+            <span className="block tablet:col-start-3 tablet:col-end-13">I'm a</span>
+            <span className="block tablet:col-start-3 tablet:col-end-13">graphic designer & dev</span>
+            <span className="block tablet:col-start-3 tablet:col-end-12">based in Belgium</span>
           </h1>
         </div>
         <div className="containerRaw">
