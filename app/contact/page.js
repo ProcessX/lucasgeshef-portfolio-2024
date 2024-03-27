35f19e40982c4ee4757147e8ba7c0cd7d284f549
ProcessX <luc2526@gmail.com>
2024-03-27 13:12:53 +0100

    ?

--- app/contact/page.js
+++ app/contact/page.js
@@ -4,19 +4,17 @@ import "./contact.css";
 function Contact() {
   return (
     <main className="bg-light-50 text-dark-900 h-[100vh]">
-      <section>
-        <div className="containerRaw pt-[160px] desktop:pt-[192px]">
+      <section className="flex flex-col h-[100vh] justify-between pb-12">
+        <div className="containerRaw pt-[160px] w-full">
           <h1 className="c-font-4xl text-right">
             <span className="block">Want to reach out?</span>
             <span className="block">Find me here</span>
           </h1>
+          <p className="inline-block float-end">Button</p>
         </div>
-        <div className="containerRaw">
-          <span>Contact button</span>
-        </div>
-        <div className="containerRaw">
-          <p className="c-font-base">Or check my socials</p>
-          <ul>
+        <div className="containerRaw w-full">
+          <p className="c-font-base text-right">Or check my socials</p>
+          <ul className="flex justify-end">
             <li>
               <span>Social icon</span>
             </li>
