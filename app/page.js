d1589308f8b6fef5270eafec35778c06b1b38666
ProcessX <luc2526@gmail.com>
2024-03-27 11:43:11 +0100

    placement for homepage done

--- app/page.js
+++ app/page.js
@@ -4,8 +4,8 @@ export default function Home() {
   return (
     <main>
 
-      <section className="min-h-[100vh]">
-        <div className="containerRaw pt-[160px] tablet:pt-[192px]">
+      <section className="min-h-[100vh] section--home laptop:flex laptop:flex-col laptop:justify-between">
+        <div className="containerRaw pt-[160px] tablet:pt-[160px] laptop:w-full">
           <h1 className="c-font-4xl mb-[88px] tablet:mb-[128px] tablet:grid tablet:grid-cols-12 laptop:mb-0">
             <span className="block mb-12 tablet:mb-0 tablet:col-span-2">Hi.</span>
             <span className="block tablet:col-start-3 tablet:col-end-13">I'm a</span>
@@ -13,9 +13,9 @@ export default function Home() {
             <span className="block tablet:col-start-3 tablet:col-end-12">based in Belgium</span>
           </h1>
         </div>
-        <div className="containerRaw">
+        <div className="containerRaw laptop:w-full laptop:mb-0">
           <div className="laptop:grid laptop:grid-cols-12">
-            <p className="c-font-base mb-8 laptop:col-span-2 laptop:mt-4">My latest work</p>
+            <p className="c-font-base mb-8 laptop:col-span-2 laptop:mb-[64px] laptop:self-end">My latest work</p>
             <ul className="laptop:col-span-10">
               <li>
                 <div>
@@ -26,6 +26,6 @@ export default function Home() {
           </div>
         </div>
       </section>
-    </main>
+    </main >
   );
 }
--- components/headerMenu/HeaderMenu.js
+++ components/headerMenu/HeaderMenu.js
@@ -3,7 +3,7 @@ import React from 'react'
 
 function HeaderMenu() {
   return (
-    <header className='fixed w-[100vw] left-0 top-0 '>
+    <header className='fixed w-[100vw] left-0 top-0 z-50'>
       <div className='containerRaw pt-6 pb-6 desktop:pt-8 desktop:pb-8 bg-dark-950 headerMenu'>
         <nav className='c-font-md flex justify-between'>
           <Link href={'/'} className='duration-150 hover:opacity-50 inline logo'>Lucas Geshef</Link>
